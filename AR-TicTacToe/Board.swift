2758a51596365d6e5aefbafc7bdf5063eb438cfe
Joseph_Yep <yewentai126@gmail.com>
2024-04-29 11:23:44 +0200

    recover to original

--- AR-TicTacToe/Board.swift
+++ AR-TicTacToe/Board.swift
@@ -1,65 +1,62 @@
 //
 //  Board.swift
-//  TicTacToe
+//  AR-TicTacToe
 //
-//  Created by Wentai Ye on Apr 24 2024
-//  Copyright © 2024 Wentai Ye. All rights reserved.
+//  Created by Bjarne Møller Lundgren on 20/06/2017.
+//  Copyright © 2017 Bjarne Møller Lundgren. All rights reserved.
 //
 
 import Foundation
 import SceneKit
 
 class Board {
-    // SCNNode representing the entire board
     let node:SCNNode
-    // Dictionary mapping square nodes to their corresponding (row, column) indices
     let nodeToSquare:[SCNNode:(Int,Int)]
-    // Dictionary mapping square positions (in string format) to their SCNVector3 positions
     let squareToPosition:[String:SCNVector3]
     
     init() {
-        // Initialize the board node
         node = SCNNode()
-        // Initialize dictionaries to map square nodes to their indices and positions
         var nodeToSquare = [SCNNode:(Int,Int)]()
         var squareToPosition = [String:SCNVector3]()
         
-        // Dimensions of the board
+        
         let length = Dimensions.SQUARE_SIZE * 4
         let height:CGFloat = Dimensions.BOARD_GRID_HEIGHT
         let width:CGFloat = Dimensions.BOARD_GRID_WIDTH
         
-        // Loop through each row of the board
+        //let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.25))
+        //sphereNode.position = SCNVector3(0, 0.4, 0)
+        //boardNode.addChildNode(sphereNode)
+        
         for l in 0..<4 {
             let lineOffset = length * 0.5 - (CGFloat(l + 1) * Dimensions.SQUARE_SIZE - Dimensions.SQUARE_SIZE * 0.5)
             
-            // Create squares for each row (except the first row)
+            // squares
             if l > 0 {
                 for r in 0..<3 {
-                    // Calculate position for each square
                     let position = SCNVector3(lineOffset + Dimensions.SQUARE_SIZE * 0.5,
                                               0.01,
-                                              CGFloat(r - 1) * Dimensions.SQUARE_SIZE)
+                                              //TODO: do a rowOffset like above for this, this is ugly!
+                        CGFloat(r - 1) * Dimensions.SQUARE_SIZE)
                     let square = (l - 1, r)
                     
-                    // Create square geometry
                     let geometry = SCNPlane(width: Dimensions.SQUARE_SIZE,
                                             height: Dimensions.SQUARE_SIZE)
                     geometry.firstMaterial!.diffuse.contents = UIColor.clear
+                    //geometry.firstMaterial!.specular.contents = UIColor.white
                     
-                    // Create SCNNode for the square and set its position
                     let squareNode = SCNNode(geometry: geometry)
                     squareNode.position = position
                     squareNode.eulerAngles = SCNVector3(-90.0.degreesToRadians, 0, 0)
                     
-                    // Add square node to the board node and update dictionaries
                     node.addChildNode(squareNode)
                     nodeToSquare[squareNode] = square
                     squareToPosition["\(square.0)x\(square.1)"] = position
                 }
             }
             
-            // Create horizontal and vertical grid lines for the current row
+            
+            // grid lines..
             let geometry = SCNBox(width: width,
                                   height: height,
                                   length: length,
@@ -71,20 +68,19 @@ class Board {
             geometry.firstMaterial?.normal.contents = UIImage(named: "Media.scnassets/scuffed-plastic-normal.png")
             geometry.firstMaterial?.ambientOcclusion.contents = UIImage(named: "Media.scnassets/scuffed-plastic-ao.png")
             
-            // Create horizontal line node and set its position
             let horizontalLineNode = SCNNode(geometry: geometry)
             horizontalLineNode.position = SCNVector3(lineOffset, height * 0.5, 0)
             node.addChildNode(horizontalLineNode)
             
-            // Create vertical line node and set its position
             let verticalLineNode = SCNNode(geometry: geometry)
             verticalLineNode.eulerAngles = SCNVector3(0, 90.0.degreesToRadians, 0)
             verticalLineNode.position = SCNVector3(0, height * 0.5, lineOffset)
             node.addChildNode(verticalLineNode)
         }
         
-        // Assign dictionaries to properties
         self.nodeToSquare = nodeToSquare
         self.squareToPosition = squareToPosition
     }
+    
+    
 }
